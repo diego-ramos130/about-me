684e2aaf87b439cdb5aa10493a755872175c513a
diego-ramos130 <diego.onethreezero@gmail.com>
2018-07-10 14:39:18 -0700

    fixed problem with input validation

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
-"use strict";
+'use strict';
 
 alert('Hello, user. My name is Diego Ramos, and this is my Program. If you want, you can answer these yes/no questions about me.');
 alert('remember, yes/no or y/n works all the same.');
-var score = 0;
+var score = 0; 
 console.log('score at start of game should be 0.', score);
 var questionOne = prompt('Do I wear glasses?');
-if (questionOne.toUpperCase === 'Y' || questionOne.toUpperCase === 'YES' ) {
+/* questionOne = 'Y'; */
+if (questionOne.toUpperCase() === 'Y' || questionOne.toUpperCase() === 'YES' ) {
   alert('Correct!');
-} else if (questionOne.toUpperCase === 'N' || questionOne.toUpperCase === 'NO') {
+  score++;
+} else if (questionOne.toUpperCase() === 'N' || questionOne.toUpperCase() === 'NO') {
   alert('Incorrect!');
 } else {
   alert('Wrong input!');
@@ -16,19 +18,21 @@ console.log('question one answer.', questionOne);
 
 
 var questionTwo = prompt('Is my nickname Peter?');
-if (questionTwo.toUpperCase === 'Y' || questionTwo.toUpperCase === 'YES') {
+if (questionTwo.toUpperCase() === 'Y' || questionTwo.toUpperCase() === 'YES') {
   alert('Incorrect!');
-} else if (questionTwo.toUpperCase === 'N' || questionTwo.toUpperCase === 'NO') {
+} else if (questionTwo.toUpperCase() === 'N' || questionTwo.toUpperCase() === 'NO') {
   alert('Correct!');
+  score++;
 } else {
   alert('Wrong input!');
 }
 console.log('question two answer.', questionTwo);
 
 var questionThree = prompt('Do I go to the Seattle Campus for CodeFellows?');
-if (questionThree.toUpperCase === 'Y' || questionThree.toUpperCase === 'YES') {
+if (questionThree.toUpperCase() === 'Y' || questionThree.toUpperCase() === 'YES') {
   alert('Correct!');
-} else if (questionThree.toUpperCase === 'N' || questionThree.toUpperCase === 'NO') {
+  score++;
+} else if (questionThree.toUpperCase() === 'N' || questionThree.toUpperCase() === 'NO') {
   alert('Incorrect!');
 } else {
   alert('Wrong input!');
@@ -37,10 +41,11 @@ console.log('question three answer.', questionThree);
 
 
 var questionFour = prompt('Do I like sweets/candy?');
-if (questionFour.toUpperCase === 'Y' || questionFour.toUpperCase === 'YES') {
+if (questionFour.toUpperCase() === 'Y' || questionFour.toUpperCase() === 'YES') {
   alert('Incorrect!');
-} else if (questionFour.toUpperCase === 'NO' || questionFour.toUpperCase === 'NO') {
+} else if (questionFour.toUpperCase() === 'N' || questionFour.toUpperCase() === 'NO') {
   alert('Correct!');
+  score++;
 } else {
   alert('Wrong input!');
 }
@@ -48,9 +53,10 @@ console.log('question four answer.', questionFour);
 
 
 var questionFive = prompt('Do I go to the Seattle Campus for CodeFellows?');
-if (questionFive.toUpperCase === 'Y' || questionFive.toUpperCase === 'YES') {
+if (questionFive.toUpperCase() === 'Y' || questionFive.toUpperCase() === 'YES') {
   alert('Correct!');
-} else if (questionFive.toUpperCase === 'N' || questionFive.toUpperCase === 'NO') {
+  score++;
+} else if (questionFive.toUpperCase() === 'N' || questionFive.toUpperCase() === 'NO') {
   alert('Incorrect!');
 } else {
   alert('Wrong input!');
@@ -59,4 +65,4 @@ console.log('question five answer.', questionFive);
 
 
 
-alert('your score was ' + score + ' out of 5.');
+/* alert('your score was ' + score + ' out of 5.'); */
